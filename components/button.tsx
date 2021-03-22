7675c0a64774352be773c488f6d3e808954bd9dd
antriksh123 <antariksha.verma@pathways.in>
2021-03-22 23:20:01 +0530

    button component

---
+++ components/button.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import cn from 'classnames'
+import styles from '../styles/button.module.scss'
+
+interface ButtonProps {
+    children: JSX.Element | JSX.Element[] | String,
+    type?: "button" | "submit" | "reset",
+    href?: string,
+    onClick: () => void,
+    variant?: 'transparent' | 'like',
+    disabled?: boolean
+}
+
+const Button = ({ children, type, href, variant, onClick, disabled }: ButtonProps): JSX.Element => {
+    const classes = cn(styles.button, styles[variant])
+    
+    if (onClick || !href) {
+        return (
+          <button className={classes} type={type === 'submit' ? 'submit' : 'button'} onClick={onClick} disabled={disabled}>
+            {children}
+          </button>
+        )
+    }
+    if (href.startsWith('/')) {
+        return (
+          <Link href={href} passHref>
+            <a>
+              <button className={classes} type={type === 'submit' ? 'submit' : 'button'} disabled={disabled}>
+                {children}
+              </button>
+            </a>
+          </Link>
+        )
+    }
+
+    return (
+        <a href={href} target="_blank" rel="noopener noreferrer" className={classes}>
+          {children}
+        </a>
+    )
+}
+
+export default Button
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ const Home = (): JSX.Element => {
         <Header />
         <div className={styles.main}>
           <h1>👋 Hi, my name is yum.</h1>
+          <p className={styles.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate odit animi quisquam, ipsam deserunt veritatis eligendi? Accusantium quos aut minus?</p>
+          <button className={styles.button}>More About Me</button>
         </div>
       </div>
     </>
