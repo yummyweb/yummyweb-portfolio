9201c6e497d2642ead75f3fc354873288b86f3d8
antriksh123 <antariksha.verma@pathways.in>
2021-03-22 22:10:45 +0530

    make header fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ const Home = (): JSX.Element => {
     <>
       <Header />
       <div className={styles.container}>
+        <h2>My Site</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet illo, repellendus quae similique eveniet, ipsam reprehenderit corporis quasi unde autem obcaecati, expedita dolorum deserunt. Placeat sunt voluptates exercitationem saepe veritatis culpa maiores at nam distinctio vitae, possimus error eos dolor eius nihil repudiandae doloribus iusto, repellat eaque aliquid animi aperiam!</p>
       </div>
     </>
   )
