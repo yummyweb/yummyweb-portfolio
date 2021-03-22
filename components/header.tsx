906a97c8697c51c68bef537ef11a0ec3e45e5d17
antriksh123 <antariksha.verma@pathways.in>
2021-03-22 21:51:28 +0530

    basci header

---
+++ components/header.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import styles from '../styles/header.module.css'
+
+const Header = (): JSX.Element => {
+    const links = [
+        {name: "Home", path: "/"},
+        {name: "About", path: "/about"},
+        {name: "Blog", path: "/blog"},
+        {name: "Projects", path: "/projects"},
+        {name: "Contact", path: "/contact"},
+    ]
+    return (
+        <header className={styles.header}>
+            <ul>
+                {links.map(link => (
+                    <li key={link.path}>
+                        <Link>{ link.name }</Link>
+                    </li>
+                ))}
+            </ul>
+        </header>
+    )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }): JSX.Element {
   return (
     <ThemeProvider defaultTheme="system">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
