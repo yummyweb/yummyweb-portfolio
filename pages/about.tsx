5606b1f01f9d61e9870d1dc80306a9167f989f62
antriksh123 <antariksha.verma@pathways.in>
2021-03-23 10:37:12 +0530

    improve about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,7 @@ const About = (): JSX.Element => {
       <div className={styles.container}>
         <Header />
         <div className={styles.main}>
-          <h2 className={styles.name}><span className={styles.hangLoose}>🤙</span> Yo, I'm yum.</h2>
+          <h1 className={styles.name}><span className={styles.hangLoose}>🤙</span> Yo, I'm yum.</h1>
           <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime asperiores, aliquid consequatur cum quos ducimus voluptatibus tenetur neque est inventore alias, praesentium ex, deleniti sunt reiciendis eius placeat itaque ipsa recusandae omnis nobis quod.
           </p>
