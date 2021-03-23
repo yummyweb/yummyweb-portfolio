909a504c56ef793e38e87c76c3227882d9678f63
antriksh123 <antariksha.verma@pathways.in>
2021-03-23 10:13:56 +0530

    fix name on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,7 @@ const About = (): JSX.Element => {
       <div className={styles.container}>
         <Header />
         <div className={styles.main}>
-          <h1 className={styles.name}><span className={styles.hangLoose}>🤙</span> Yo, my name is Antariksh Verma.</h1>
+          <h2 className={styles.name}><span className={styles.hangLoose}>🤙</span> Yo, I'm yum.</h2>
           <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime asperiores, aliquid consequatur cum quos ducimus voluptatibus tenetur neque est inventore alias, praesentium ex, deleniti sunt reiciendis eius placeat itaque ipsa recusandae omnis nobis quod.
           </p>
