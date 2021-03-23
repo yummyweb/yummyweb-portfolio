e94d045ba4b5af6898bd73eb0a4babc3da08e4f1
antriksh123 <antariksha.verma@pathways.in>
2021-03-23 13:03:02 +0530

    added transitions

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,27 @@
 import Header from '../components/header'
 import styles from '../styles/about.module.scss'
 import Image from 'next/image'
+import Transition from '../components/transition'
 
 const About = (): JSX.Element => {
   return (
     <>
       <div className={styles.container}>
         <Header />
-        <div className={styles.main}>
-          <div className={styles.logoContainer}>
-            <Image className={styles.logo} width="100" height="100" alt="Yum" src="/yum_logo.png" />
+        <Transition>
+          <div className={styles.main}>
+            <div className={styles.logoContainer}>
+              <Image className={styles.logo} width="100" height="100" alt="Yum" src="/yum_logo.png" />
+            </div>
+            <h1 className={styles.name}><span className={styles.hangLoose}>🤙</span> Yo, I'm yum.</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime asperiores, aliquid consequatur cum quos ducimus voluptatibus tenetur neque est inventore alias, praesentium ex, deleniti sunt reiciendis eius placeat itaque ipsa recusandae omnis nobis quod.
+            </p>
+            <p>
+                <a href="/" className="link"><span data-content="Lorem ipsum" className="dotted">Lorem ipsum</span></a> dolor sit amet consectetur, adipisicing elit. Quod, <a className="link" href="/"><span className="dotted" data-content="necessitatibus?">necessitatibus?</span></a>
+            </p>
           </div>
-          <h1 className={styles.name}><span className={styles.hangLoose}>🤙</span> Yo, I'm yum.</h1>
-          <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime asperiores, aliquid consequatur cum quos ducimus voluptatibus tenetur neque est inventore alias, praesentium ex, deleniti sunt reiciendis eius placeat itaque ipsa recusandae omnis nobis quod.
-          </p>
-          <p>
-              <a href="/" className="link"><span data-content="Lorem ipsum" className="dotted">Lorem ipsum</span></a> dolor sit amet consectetur, adipisicing elit. Quod, <a className="link" href="/"><span className="dotted" data-content="necessitatibus?">necessitatibus?</span></a>
-          </p>
-        </div>
+        </Transition>
       </div>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Button from '../components/button'
 import Header from '../components/header'
 import HomeBlog from '../components/home/blog'
+import Transition from '../components/transition'
 import styles from '../styles/index.module.scss'
 
 const Home = (): JSX.Element => {
@@ -8,19 +9,21 @@ const Home = (): JSX.Element => {
     <>
       <div className={styles.container}>
         <Header />
-        <div className={styles.main}>
-          <h1><span className={styles.wave}>👋</span> Hi, my name is yum.</h1>
-          <p className={styles.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate odit animi quisquam, ipsam deserunt veritatis eligendi? Accusantium quos aut minus?</p>
-          <br/>
-          <Button href="/about">More About Me</Button>
-          <hr/>
-          <div className={styles.blogPosts}>
-            <h2>Check out my latest posts</h2>
-            <div>
-              <HomeBlog />
+        <Transition>
+          <div className={styles.main}>
+            <h1><span className={styles.wave}>👋</span> Hi, my name is yum.</h1>
+            <p className={styles.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate odit animi quisquam, ipsam deserunt veritatis eligendi? Accusantium quos aut minus?</p>
+            <br/>
+            <Button href="/about">More About Me</Button>
+            <hr/>
+            <div className={styles.blogPosts}>
+              <h2>Check out my latest posts</h2>
+              <div>
+                <HomeBlog />
+              </div>
             </div>
           </div>
-        </div>
+        </Transition>
       </div>
     </>
   )
