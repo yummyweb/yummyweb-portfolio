6a6a25385caee75814743c36d2256848a6c307ea
antriksh123 <antariksha.verma@pathways.in>
2021-03-25 11:55:41 +0530

    configure mdx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const withMDX = require('@next/mdx')()
 
-module.exports = {
+module.exports = withMDX({
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, 'styles')]
   },
-}
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
+})
