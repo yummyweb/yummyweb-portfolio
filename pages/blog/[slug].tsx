c906da7967de0546200724c90b5740ac048baa1b
antriksh123 <antariksha.verma@pathways.in>
2021-04-26 15:38:14 +0530

    create blog post slug and route

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,36 @@
+import { getFiles, getFileBySlug } from '../../lib/mdx'
+import hydrate from 'next-mdx-remote/hydrate'
+import BlogLayout from '../../layout/blog'
+import MDXComponents from '../../components/MDXComponents'
+import Tweet from '../../components/tweet'
+
+const BlogPost = ({ mdxSource, tweets, frontMatter }) => {
+    const content = hydrate(mdxSource, {
+        components: {
+          ...MDXComponents
+        }
+    });
+
+    return <BlogLayout frontMatter={frontMatter}>{content}</BlogLayout>;
+}
+
+export async function getStaticPaths() {
+    const posts = await getFiles('blog');
+  
+    return {
+      paths: posts.map((p) => ({
+        params: {
+          slug: p.replace(/\.mdx/, '')
+        }
+      })),
+      fallback: false
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const post = await getFileBySlug('blog', params.slug);
+  
+    return { props: { ...post } }
+}
+
+export default BlogPost
