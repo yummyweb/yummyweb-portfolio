680e9dddde224f006dafee11cef08471b9e39382
antriksh123 <antariksha.verma@pathways.in>
2021-04-26 18:38:51 +0530

    add generate rss script

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import HomeBlog from '../components/home/blog'
 import NowPlaying from '../components/now-playing'
 import Page from '../components/page'
 import styles from '../styles/index.module.scss'
+import { getFiles } from '../lib/mdx'
 
-const Home = (): JSX.Element => {
+interface IProps {
+  posts: string[]
+}
+
+const Home = ({ posts }: IProps): JSX.Element => {
+  console.log(posts[0].split('.')[0])
+
   return (
     <Page active="Home">
         <div className="main">
@@ -12,14 +18,22 @@ const Home = (): JSX.Element => {
           <hr />
           <div className={styles.blogPosts}>
             <h2>Check out my latest posts</h2>
-            <div>
-              <HomeBlog />
+            <div className={styles.blog}>
+              <div className={styles.post}>
+                <h3>{ posts[0].split('.')[0] }</h3>
+              </div>
             </div>
           </div>
           <NowPlaying />
         </div>
     </Page>
   )
+}
+
+export async function getStaticProps({ params }) {
+  const posts = await getFiles('blog')
+
+  return { props: { posts } }
 }
 
 export default Home
---
+++ scripts/generate-rss.ts
@@ -0,0 +1,34 @@
+const { promises: fs } = require('fs')
+const path = require('path')
+const RSS = require('rss')
+const matter = require('gray-matter')
+
+async function generate() {
+  const feed = new RSS({
+    title: 'yum',
+    site_url: 'https://yummyweb.vercel.app',
+    feed_url: 'https://yummyweb.vercel.app/feed.xml',
+  })
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'data', 'blog'))
+
+  await Promise.all(
+    posts.map(async name => {
+      const content = await fs.readFile(path.join(__dirname, '..', 'data', 'blog', name))
+      const frontmatter = matter(content)
+
+      feed.item({
+        title: frontmatter.data.title,
+        url: 'https://yummyweb.vercel.app/blog/' + name.replace(/\.mdx?/, ''),
+        date: frontmatter.data.publishedAt,
+        description: frontmatter.data.summary,
+      })
+    }),
+  )
+
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+}
+
+generate()
+
+export {}
