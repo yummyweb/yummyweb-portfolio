4d192fda5b8c41340d77d77f822e1acfdd3e9dfd
antriksh123 <antariksha.verma@pathways.in>
2021-04-26 16:21:15 +0530

    add default theme dark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }): JSX.Element {
   return (
-    <ThemeProvider defaultTheme="system">
+    <ThemeProvider defaultTheme="dark">
       <Head>
         <title>Yum - Software Dev</title>
         <link rel="icon" href="/favicon.ico" />
