ee22de64ac7b20404d24d732522ba97104126e9e
antriksh123 <antariksha.verma@pathways.in>
2021-04-26 19:51:16 +0530

    blog posts are now visible!

--- components/post-list.tsx
+++ components/post-list.tsx
@@ -1,5 +1,8 @@
 import type { BlogPosts } from '../pages/blog'
 import styles from '../styles/post-list.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+import { parseISO, format } from 'date-fns'
 
 interface IProps {
     posts: BlogPosts
@@ -8,7 +11,31 @@ interface IProps {
 const PostList = ({ posts }: IProps): JSX.Element => {
     return (
         <ul className={styles.posts}>
-
+            {posts.length === 0 && <p className={styles.noResults}>🧐 Oops... no posts found!</p>}
+            {posts.map(post => {
+                const {
+                    meta: { summary, title, readingTime: readTime, publishedAt, image },
+                } = post
+                const slug = post.filePath.replace(/\.mdx?$/, '')
+                return (
+                    <li key={post.filePath}>
+                        <Link as={`/blog/${slug}`} href="/blog/[slug]">
+                            <a aria-label={title}>
+                                <span className={styles.imgWrapper}>
+                                    <Image width={2024} height={1012} src={image} alt={title} /> 
+                                </span>
+                            </a>
+                        </Link>
+                        <Link as={`/blog/${slug}`} href="/blog/[slug]">
+                            <a className={styles.title}>{title}</a>
+                        </Link>
+                        <p className={styles.summary}>{summary}</p>
+                        <p className={styles.meta}>
+                            Published on <time dateTime={publishedAt}>{format(parseISO(publishedAt), 'MMMM dd, yyyy')}</time> &middot; {readTime.text}
+                        </p>
+                    </li>
+                )
+            })}
         </ul>
     )
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,23 @@
 import { useState, useCallback } from 'react'
 import Page from '../components/page'
 import Search from '../components/search'
+import PostList from '../components/post-list'
 import styles from '../styles/blog.module.scss'
 import { Meta } from '../types'
+import readingTime from 'reading-time'
+import fs from 'fs'
+import matter from 'gray-matter'
+import { GetStaticProps } from 'next'
+import path from 'path'
+import { postFilePaths, POSTS_PATH } from '../utils/mdxUtils'
 
 export type BlogPosts = Array<{ content: string; filePath: string; meta: Meta }>
 
-type BlogProps = {
+interface IProps {
     posts: BlogPosts
 }
 
-const Blog = (): JSX.Element => {
+const Blog = ({ posts }: IProps): JSX.Element => {
     const [search, setSearch] = useState('')
 
     return (
@@ -21,9 +28,25 @@ const Blog = (): JSX.Element => {
                 <div className={styles.search}>
                     <Search value={search} onChange={e => setSearch(e.target.value)} placeholder={"Search blog posts"} />
                 </div>
+                <PostList posts={posts} />
             </div>
         </Page>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const posts = postFilePaths.map(filePath => {
+      const source = fs.readFileSync(path.join(POSTS_PATH, filePath))
+      const { content, data } = matter(source)
+  
+      return {
+        content,
+        meta: { ...data, readingTime: readingTime(content) },
+        filePath,
+      }
+    })
+  
+    return { props: { posts } }
 }
 
 export default Blog
