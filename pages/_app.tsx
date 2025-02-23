b10c16e1a7a6ade10cfd6fba9e816007e2a18d5a
yummyweb <antriksh.timepass@gmail.com>
2025-02-23 17:48:00 +0800

    updated version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }): JSX.Element {
   return (
+    //@ts-ignore: sdc
     <ThemeProvider defaultTheme="dark">
       <Head>
         <title>Antariksh Verma - Personal Blog</title>
